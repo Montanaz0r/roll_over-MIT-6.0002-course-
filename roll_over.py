e660d65f97f9a0b7960734d6e5f307ca6866dc21
Montanaz0r <52318290+Montanaz0r@users.noreply.github.com>
2019-09-16 20:57:43 +0200

    Update roll_over.py

--- roll_over.py
+++ roll_over.py
@@ -1,6 +1,6 @@
 # roll_over_f.py - checks how to maximize value of equation by setting up certain variables.
 
-#Score = ((60 – (a+b+c+d+e))*F + a*ps1 + b*ps2 + c*ps3 + d*ps4 + e*ps5
+# Score = ((60 – (a+b+c+d+e))*F + a*ps1 + b*ps2 + c*ps3 + d*ps4 + e*ps5
 # Objective:
 # Given values for F, ps1, ps2, ps3, ps4, ps5
 # Find values for a, b, c, d, e that maximize score
